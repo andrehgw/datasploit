517c5fc840712cf09cf2faf8906631e959b5392d
Andre Fiedler <andre@balticnetwork.de>
2018-05-22 23:04:18 +0200

    indents corrected

--- vault.py
+++ vault.py
@@ -6,17 +6,17 @@ def get_key(a):
     config_path = os.path.dirname(__file__)
     config_file = "%s/config.py" % config_path
     if os.path.exists(config_file):
-      config = ConfigObj(config_file)
-      try:
-          if config[a] != '':
-              return config[a]
-          else:
-              msg = "[-] " + a + " not configured"
-              print colored(msg, 'yellow')
-              return None
-      except:
-          msg = "[!] " + a + " not present"
-          print colored(msg, 'yellow')
-          return None
+        config = ConfigObj(config_file)
+        try:
+            if config[a] != '':
+                return config[a]
+            else:
+                msg = "[-] " + a + " not configured"
+                print colored(msg, 'yellow')
+                return None
+        except:
+            msg = "[!] " + a + " not present"
+            print colored(msg, 'yellow')
+            return None
     else:
-      print colored("[-] Error opening config file", 'yellow')
+        print colored("[-] Error opening config file", 'yellow')
