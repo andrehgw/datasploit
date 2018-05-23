bd1912b276c8564f651e1c2edb80fe8ffc0733a0
root <root@kali>
2018-05-23 16:05:45 +0200

    finished first working version of module

--- username/username_facebook.py
+++ username/username_facebook.py
@@ -1,12 +1,65 @@
 #!/usr/bin/env python
+"""
+Query Facebook graph search API for user. 
+
+    https://graph.facebook.com/maik.kunze?access_token=<ACCESS_TOKEN>
+    
+    --> get <ACCESS_TOKEN> from: https://developers.facebook.com/tools/accesstoken/
+
+Searching for user string with the graph API isn't possible, 
+but there's a trick with the error message:
+
+response user exists:
+---------------------
+    {
+       "error": {
+          "message": "(#803) Cannot query users by their username (<username>)",
+          "type": "OAuthException",
+          "code": 803,
+          "fbtrace_id": "ChAg6PPZ+RN"
+       }
+    }
+
+response if user doesn't exist:
+-------------------------------
+    {
+      "error": {
+        "message": "(#803) Some of the aliases you requested do not exist: maik.kunzeddddd",
+        "type": "OAuthException",
+        "code": 803,
+        "fbtrace_id": "FKQiim56y8K"
+      }
+    }
+
+So, with parsing the error message we can suspect the user exist.
+
+"""
+
+__author__ = "Andre Fiedler <andre@balticnetwork.de>"
+__copyright__ = "Copyright"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Andre Fiedler"
+__email__ = "andre@balticnetwork.de"
+__status__ = "Production"
+
 
 import base
 import vault
 import sys
+
+import requests
+import json
+
 from termcolor import colored
 
 # Control whether the module is enabled or not
 ENABLED = True
+
+
+facebook_access_token=vault.get_key('facebook_access_token')
+
+fb_api_url = "https://graph.facebook.com"
 
 class style:
     BOLD = '\033[1m'
@@ -14,26 +67,62 @@ class style:
 
 def banner():
     # Write a cool banner here
-    print colored(style.BOLD + '\n[+] Checking if Facebook username exists\n' + style.END, 'blue')
+    print colored(style.BOLD + '\n[+] Checking Facebook\n' + style.END, 'blue')
     pass
 
 
 def main(username):
+    userstats = {}
+    
     user_information = []
+    user_error = []
+    
     facebook_access_token = vault.get_key('facebook_access_token')
     
     if facebook_access_token != None and len(facebook_access_token) > 0:
-        user_information.append("TODO: implement facebook query for %s..." % username)
+        try:
+            graph_url = '%s/%s?access_token=%s' % (fb_api_url,username,facebook_access_token)
+            req = requests.get(graph_url)
+            data = json.loads(req.text)
+            
+            if 'error' in data and len(data['error']) > 0:
+                errordict = data['error']
+                if 'message' in errordict and len(errordict['message']) > 0: 
+                    errormessage = errordict['message']
+                    # user exists ;-)
+                    if errormessage.find('query users by their username') != -1:
+                        user_information.append("user alias '%s' exists on Facebook" % username)
+                    else: 
+                        user_error.append("'%s' not found on Facebook" % username)
+                else:
+                    user_error.append("no tag 'message'")     
+            else:
+                user_error.append("no tag 'error'")
+                           
+        except Exception, e:
+            user_error.append("Error: %s" % repr(e) )
+        
     else: 
-        user_information.append("no Facebook access token configured")
+        user_error.append("no Facebook access token configured")
     
-    return user_information
+    userstats['user_information'] = user_information
+    userstats['user_error'] = user_error
+    
+    return userstats
 
 
 def output(data, username=""):
-    # Use the data variable to print out to console as you like
-    for i in data:
-        print i
+    # output all errors
+    if 'user_error' in data and len(data['user_error']) > 0:
+        for cerror in data['user_error']:
+            print colored(style.BOLD + "[!] Error: " + cerror + style.END, 'red')
+            
+    # output all user information
+    if 'user_information' in data and len(data['user_information']) > 0:
+        for cinfo in data['user_information']:
+            print colored(style.BOLD + cinfo + style.END, 'green')
+            
+    
 
 
 if __name__ == "__main__":
